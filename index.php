49f177fa6ecfea1869c1907c2d09627089893ea9
pedrog <pedrog@pedrog-GA-MA74GM-S2>
2014-11-23 13:03:02 +0000

    Este debería ser una rama más simple del "AprendoMVC" Tiene un problema con la activación de cuenta mediante email ya que no coge el segundo parámetro (el segundo argumento de la URL) v0.01 Añadido posts en portada limitados a 3

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Versión sin plantillas, ni módulos, ni class image
  */
 
 ini_set('display_errors', 1);
